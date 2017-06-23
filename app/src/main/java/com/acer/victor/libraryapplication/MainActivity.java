876306bfb8f4d9bf49c5b28fdac784580195a4ea
Drawn0-0 <shuilengyue@aliyun.com>
2017-06-23 23:54:40 +0800

    Initial commit

---
+++ app/src/main/java/com/acer/victor/libraryapplication/MainActivity.java
@@ -0,0 +1,17 @@
+package com.acer.victor.libraryapplication;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.acer.victor.loglibrary.LogUtil;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        LogUtil.i("tag","abc");
+        LogUtil.LEVEL = LogUtil.NOTHING;
+    }
+}
