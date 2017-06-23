58ebc2648ba49121b6e6d8c137c4eceee9261de3
Drawn0-0 <shuilengyue@aliyun.com>
2017-06-24 00:18:07 +0800

    repair error

--- app/src/main/java/com/acer/victor/libraryapplication/MainActivity.java
+++ app/src/main/java/com/acer/victor/libraryapplication/MainActivity.java
@@ -13,5 +13,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         LogUtil.i("tag","abc");
         LogUtil.LEVEL = LogUtil.NOTHING;
+        LogUtil.i("tag","def");
     }
 }
